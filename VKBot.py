3e9965ace3aca1ee62454867f043074dcfbab29b
Владимир Норакидзе <vladimir@MacBook-Pro-Vladimir.local>
2019-05-11 19:16:35 +0300

    VKbot new

--- VKBot.py
+++ VKBot.py
@@ -1,5 +1,8 @@
 import requests
 import random
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
 TOKEN = "bafa83804e118b05e67670d10ac9993b98369fb6129c353e85efef71dfa0070bf43b3d2b551ed67d9d0e8"  # for hakaton
 url = "https://api.vk.com/method/"
 
@@ -30,5 +33,22 @@ def chat_info(id):
     print(response)
 
 
-print(send_msg_to_user(61118227, str(random.random()*1000)))
-# print(send_msg_to_chat(200, str(random.random()*1000)))
+# print(send_msg_to_user(61118227, str(random.random()*1000)))
+
+vk_session = vk_api.VkApi(token='токен с доступом к сообщениям и фото')
+longpoll = VkLongPoll(vk_session)
+
+vk = vk_session.get_api()
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+    # Слушаем longpoll, если пришло сообщение то:
+        print(send_msg_to_user(event.user_id, message="ПРИВЕТ :3"))
+
+    #     if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
+    #         if event.from_user: #Если написали в ЛС
+    #             # Отправляем сообщение
+    #             vk.messages.send(user_id=event.user_id, message='Ваш текст')
+    #         elif event.from_chat: #Если написали в Беседе
+    #             # Отправляем собщение
+    #             vk.messages.send( chat_id=event.chat_id, message='Ваш текст')
